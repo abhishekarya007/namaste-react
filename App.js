b8435f3858f3211bf09026b2a91bc27c2638f53a
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-06-17 23:06:06 +0530

    added some notes and first component creation using JSX

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 // Creating HTML tag using JS
 
-{/* <script>
+{
+  /* <script>
   const heading = document.createElement('h1') heading.innerHTML = 'Hii this is
   Abhishek Kumar' const rootElement = document.getElementById('root')
   rootElement.appendChild(heading)
-</script>; */}
+</script>; */
+}
 
 // Creating HTML tag using React.createElement
 // It takes three arguments ( tag, attributes, child/children)
@@ -15,6 +17,18 @@ const heading = React.createElement("h1", {}, "hello world !!!"); // JS OBJ
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// ## JSX is HTML like syntax while is used to write react code to make it readable as writing the whole code using React.createElement
+//     will not be readable
+// ## Babel converts JSX - > React.createElement - > ReactElement - > JS onject  - > Reandered by ReactDOM
+// ## Babel is not part of react and also it is not created by facebook unlike React
+
+const headingJsx = (
+  <div>
+    <h1 className="name">Namaste React</h1>
+  <h1 className="name">Namaste React</h1>
+  </div>
+
+)
 
 //root.render will convert this obj to html tag and put it in DOM ( Document Object Model) //////important\\\\\\
 // root.render(heading);
@@ -28,13 +42,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 </div> */
 }
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h11 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h11 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ])
+// );
 
-root.render(parent);
+root.render(headingJsx);
