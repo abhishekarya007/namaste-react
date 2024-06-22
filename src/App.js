6d753aba0e86b6a257ecd66c754c34759a8e546e
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-06-22 13:38:56 +0530

    added shimmer UI and logic to get top restra

--- src/App.js
+++ src/App.js
@@ -4,38 +4,45 @@ import Header from "./components/Header";
 import RestroCard from "./components/RestroCard";
 import { retroInfoApiUrl } from "./Utils/Constants";
 import { useState, useEffect } from "react";
+import ShimmerUi from "./components/ShimmerUi";
 
 const AppLayout = () => {
   const [restroList, setRetroList] = useState([]);
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(retroInfoApiUrl)
-    const json = await data.json()
-    setRetroList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
+    const data = await fetch(retroInfoApiUrl);
+    const json = await data.json();
+    setRetroList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   const filterCards = () => {
-    console.log(restroList)
-    let filteredRestro = restroList.filter(restaurant => {
-      return restaurant.info.avgRating > 4.5
-    })
+    console.log(restroList);
+    let filteredRestro = restroList.filter((restaurant) => {
+      return restaurant.info.avgRating > 4.5;
+    });
 
-    setRetroList(filteredRestro)
-  }
+    setRetroList(filteredRestro);
+  };
 
   return (
     <>
       <Header />
-      <div onClick={filterCards} className="search">Search</div>
-      <div className="restro-container">
-        {restroList.map((restaurant) => (
-          <RestroCard key={restaurant.info.id} restroInfo={restaurant} />
-        ))}
-      </div>
+      <button className="btn" onClick={filterCards}>Get Top Restaurants</button>
+      {restroList.length === 0 ? (
+        <ShimmerUi/>
+      ) : (
+        <div className="restro-container">
+          {restroList.map((restaurant) => (
+            <RestroCard key={restaurant.info.id} restroInfo={restaurant} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,26 @@
+const ShimmerUi = () => {
+  return (
+    <div className="restro-container">
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+      <div className="shimmerUi"></div>
+    </div>
+  );
+};
+
+export default ShimmerUi;
